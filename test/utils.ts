8b95fc85f89e4be7f8bddf4397143431cf7a9df5
Luca <61409445+melusc@users.noreply.github.com>
2024-11-12 09:27:31 +0100

    Sort imports

--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,5 @@
-import test from 'node:test';
 import assert from 'node:assert/strict';
+import test from 'node:test';
 
 import {defaultOptions, normalizeOptions, type Options} from '../src/utils.js';
 
